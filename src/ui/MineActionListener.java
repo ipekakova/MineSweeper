f4747c23e366639a965ee48afb16c8734c0e14df
ipekakova <iakova97@gmail.com>
2018-12-07 00:42:30 +0300

    Mine Sweeper Agent Completed

---
+++ src/ui/MineActionListener.java
@@ -0,0 +1,25 @@
+package ui;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MineActionListener implements ActionListener {
+
+	public MineActionListener() {
+		
+	}
+
+	public void actionPerformed(ActionEvent evt) {
+		Object srcObj = evt.getSource();
+		if(srcObj instanceof MineFieldButton){
+			MineFieldButton srcButton = (MineFieldButton)srcObj;
+			System.out.println("Button Clicked!!! ");
+			/*if(srcButton.getAssociatedSquare().isContainsMine()){
+				System.out.println("KABOOOM!!!! KABOOOM!!!! KABOOOM!!!! U R KILLED...");
+			}*/
+		}else{
+			System.err.println("MineActionListener Action Listener works only for Buttons of type MineFieldButton");
+		}
+		
+	}
+
+}
---
+++ src/ui/MineFieldButton.java
@@ -0,0 +1,47 @@
+package ui;
+
+import javax.swing.Action;
+import javax.swing.Icon;
+import javax.swing.JButton;
+
+import bean.Square;
+
+public class MineFieldButton extends JButton {
+	
+	private Square associatedSquare;
+	
+	
+	
+	public Square getAssociatedSquare() {
+		return associatedSquare;
+	}
+
+	public void setAssociatedSquare(Square associatedSquare) {
+		this.associatedSquare = associatedSquare;
+	}
+
+	public MineFieldButton() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public MineFieldButton(Icon icon) {
+		super(icon);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MineFieldButton(String text) {
+		super(text);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MineFieldButton(Action a) {
+		super(a);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MineFieldButton(String text, Icon icon) {
+		super(text, icon);
+		// TODO Auto-generated constructor stub
+	}
+
+}
